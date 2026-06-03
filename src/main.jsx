4529f5f60e979f412b864e2c5971fa5424e9e12d
DanielJH1135 <danieljhlee1135@gmail.com>
2026-06-03 22:36:06 +0900

    테일윈드,css추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
 // 만약 프로젝트에 전역 css 파일이 있다면 여기에 import 하시면 됩니다.
 // 예: import './index.css'
